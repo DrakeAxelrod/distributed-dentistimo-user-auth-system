c46ae4bf9982605c14e0dd4a825ca6a8ad351d53
Drake Axelrod <drake@draxel.io>
2021-12-08 22:01:20 +0100

    Fix login
    correctly logs you in and updates the state of the application

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,53 +4,53 @@ const bcrypt = require("bcrypt");
 const SALT = 5;
 const { log } = console;
 
+const responsePath = "api/gateway/users";
 
-const responsePath = "api/gateway/users"
+const hashPass = async (password) => await bcrypt.hash(password, SALT);
+const checkPass = async (reqPass, pass) => await bcrypt.compare(reqPass, pass);
 
-
-const hashPass = async (password) => await bcrypt.hash(password, SALT)
-const checkPass = async (reqPass, pass) => await bcrypt.compare(reqPass, pass)
-
-const login = async ({ email, password}) => { 
-  const result = await model.findOne({email: email}).then( async foundUser => {
-    if (!foundUser) 
-    {
-      return { authenticated: false, message: "User not found"} 
-    }
-    const isCorrectPass = await checkPass(password, foundUser.password)
-    if (isCorrectPass) 
-    {
-      return { authenticated: true, message: foundUser }
-    }
-    else 
-    {
-        return { authenticated: false, message: "incorrect password" }
-    }
-  })
-  return result
-}
+const login = async (m) => {
+  console.log(m.email);
+  const result = await model
+    .findOne({ email: m.email })
+    .then(async (foundUser) => {
+      if (!foundUser) {
+        return { authenticated: false, message: "User not found" };
+      }
+      const isCorrectPass = await checkPass(m.password, foundUser.password);
+      if (isCorrectPass) {
+        return { authenticated: true, message: foundUser };
+      } else {
+        return { authenticated: false, message: "incorrect password" };
+      }
+    });
+  return result;
+};
 const register = async (topic, user) => {
-  user.password = await hashPass(user.password)
-  const res = await model.create(user)
+  user.password = await hashPass(user.password);
+  const res = await model.create(user);
   const new_user = {
-    _id: res._id, 
+    _id: res._id,
     email: res.email,
     name: { first: res.name.first, last: res.name.last },
     personalNumber: res.personalNumber,
     phone: res.phone,
     bookings: res.bookings,
-  }
-  console.log(new_user)
-  client.publish(`${responsePath}/${topic}`, JSON.stringify(new_user))
-}
+  };
+  console.log(new_user);
+  client.publish(`${responsePath}/${topic}`, JSON.stringify(new_user));
+};
 
 const findAll = async () => {
-  const res = await model.find().then((res) => res).catch((err) => log(err));
-  return res
+  const res = await model
+    .find()
+    .then((res) => res)
+    .catch((err) => log(err));
+  return res;
 };
 
 module.exports = {
   findAll: findAll,
   login: login,
-  register: register
+  register: register,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
-const client = require("../utils/Client")
+const client = require("../utils/Client");
 const controllers = require("../controllers");
 const { log } = console;
 
 // set up base path
-const basePath = "api/users"
-const responsePath = "api/gateway/users"
+const basePath = "api/users";
+const responsePath = "api/gateway/users";
 
-client.subscribe(basePath)
+client.subscribe(basePath);
 // add topics to listen to
 const topics = [
   { topic: "login", qos: 0 },
@@ -14,39 +14,51 @@ const topics = [
   //{ topic: "frontend", qos: 0 },
 ];
 // loop subscribe
-topics.forEach(route => {
+topics.forEach((route) => {
   //log(basePath + "/" + route.topic);
-  client.subscribe(basePath + "/" + route.topic, { qos: route.qos })
+  client.subscribe(basePath + "/" + route.topic, { qos: route.qos });
 });
 
 // emit the topic
 client.on("message", (t, m) => {
-  const msg = JSON.parse(m.toString())
+  const msg = JSON.parse(m.toString());
   const topic = t.replace(basePath + "/", ""); // api/users/login -> login
   //log(topic)
   if (topic === "api/users") {
-    client.emit("/")
+    client.emit("/");
   } else {
-    client.emit(topic, topic,  msg)
+    client.emit(topic, topic, msg);
   }
 });
 
 // this is where routes go
 // so you listen for the topic and call relevant controller functions
 client.on("login", async (t, m) => {
-  const res = await controllers.users.login(t, m)
-   client.publish(responsePath + "/login", JSON.stringify(res));
-})
-client.on("register", (t, m) => {
-  controllers.users.register(t, m)
+  const result = await controllers.users.login(m);
+  const data = {
+    authenticated: result.authenticated,
+    message: {
+      _id: result.message._id,
+      email: result.message.email,
+      name: {
+        first: result.message.name.first,
+        last: result.message.name.last,
+      },
+      personalNumber: result.message.personalNumber,
+      phone: result.message.phone,
+    },
+  };
+  console.log(data);
+  client.publish(responsePath + "/login", JSON.stringify(data));
 });
-client.on("/", async () => {
-  const res = await controllers.users.findAll()
+client.on("register", (t, m) => {
+  controllers.users.register(t, m);
+});
+client.on("all", async () => {
+  const res = await controllers.users.findAll();
   // log(res)
   // send back to gateway
-  client.publish(responsePath, Buffer.from(res))
-})
-
-
+  client.publish(responsePath, Buffer.from(res));
+});
 
 module.export = client;
